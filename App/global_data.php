39f5fb215a53745ec8e6da9feb3946f41bf93efd
tanszhe <1018595261@qq.com>
2018-11-08 19:59:55 +0800

    ws router

--- App/global_data.php
+++ App/global_data.php
@@ -9,6 +9,8 @@ define('_APP_PATH_VIEW_',__DIR__.'/View');
 
 require __DIR__.'/../vendor/autoload.php';
 require __DIR__.'/../vendor/lizhichao/one/src/run.php';
+require _APP_PATH_ . '/config.php';
+\One\Http\Router::loadRouter();
 
 \One\Swoole\OneServer::setConfig(config('global_data'));
 
--- App/index.php
+++ App/index.php
@@ -6,11 +6,13 @@ define('_APP_PATH_VIEW_', __DIR__ . '/View');
 
 require __DIR__.'/../vendor/autoload.php';
 require __DIR__.'/../vendor/lizhichao/one/src/run.php';
-
-$req = new \One\Http\Request();
-$res = new \One\Http\Response($req);
+require _APP_PATH_ . '/config.php';
 
 try {
+    \One\Http\Router::loadRouter();
+    $req = new \One\Http\Request();
+    $res = new \One\Http\Response($req);
+
     $router = new \One\Http\Router();
     list($req->class, $req->method, $mids, $action, $req->args) = $router->explain($req->method(), $req->uri(), $req, $res);
     $f = $router->getExecAction($mids, $action, $res);
