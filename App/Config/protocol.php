6f0c95ab9d5765ec17cad5a399b5f3bedce97b45
tanszhe <1018595261@qq.com>
2018-11-15 16:14:20 +0800

    demo

--- App/Config/protocol.php
+++ App/Config/protocol.php
@@ -17,23 +17,23 @@ return [
         'ip' => '0.0.0.0',
         'set' => [
         ],
-        'global_data' => [ //globalData 配置
-            'ip' => '127.0.0.1',
-            'port' => 9086,
-            'protocol' => \One\Protocol\Frame::class,
-            'action' => \App\GlobalData\Client::class,
-            'time' => 0.5,
-            'async' => 0,
-            'set' => [
-                'open_length_check'     => 1,
-                'package_length_type'   => 'N',
-                'package_length_offset' => 0,
-                'package_body_offset'   => 0,
-//                'open_length_check' => 1,
-//                'package_length_func' => '\One\Protocol\Frame::length',
-//                'package_body_offset' => \One\Protocol\Frame::HEAD_LEN,
-
-            ]
-        ]
+//        'global_data' => [ //globalData 配置
+//            'ip' => '127.0.0.1',
+//            'port' => 9086,
+//            'protocol' => \One\Protocol\Frame::class,
+//            'action' => \App\GlobalData\Client::class,
+//            'time' => 0.5,
+//            'async' => 0,
+//            'set' => [
+//                'open_length_check'     => 1,
+//                'package_length_type'   => 'N',
+//                'package_length_offset' => 0,
+//                'package_body_offset'   => 0,
+////                'open_length_check' => 1,
+////                'package_length_func' => '\One\Protocol\Frame::length',
+////                'package_body_offset' => \One\Protocol\Frame::HEAD_LEN,
+//
+//            ]
+//        ]
     ]
 ];
