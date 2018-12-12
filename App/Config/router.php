b02869f82af08b04fc83365d0a84fd65f612bc63
tanszhe <1018595261@qq.com>
2018-12-12 10:31:57 +0800

    demo

--- App/Config/router.php
+++ App/Config/router.php
@@ -3,14 +3,30 @@
 /**
  * 路由设置
  */
+
 use One\Http\Router;
+
 Router::get('/', \App\Controllers\IndexController::class . '@index');
 
+Router::get('/users', [
+    'use' => \App\Controllers\IndexController::class . '@userList',
+    'as'  => 'users'
+]);
+Router::get('/user/{id}', [
+    'use' => \App\Controllers\IndexController::class . '@user',
+    'as'  => 'user'
+]);
+
+Router::get('/a/{name}',\App\Controllers\IndexController::class . '@aa');
+
+Router::get('/b/`^log\_\d+$`',\App\Controllers\IndexController::class . '@zz');
+
 // orm 列子
-require __DIR__.'/../Test/Orm/router.php';
+require __DIR__ . '/../Test/Orm/router.php';
 
 // ws 列子
-require __DIR__.'/../Test/WebSocket/router.php';
+require __DIR__ . '/../Test/WebSocket/router.php';
 
-require  __DIR__.'/../Test/MixPro/router.php';
+// 混合协议通讯
+require __DIR__ . '/../Test/MixPro/router.php';
 
--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -8,9 +8,28 @@ class IndexController extends Controller
 {
     public function index()
     {
-        return 'a';
+        $this->response->write('hello');
     }
 
+    public function home()
+    {
+        echo router('user', ['id' => 100]);
+    }
+
+    public function user($id)
+    {
+        echo $id;
+    }
+
+    public function aa($name)
+    {
+        return $name;
+    }
+
+    public function zz($aa)
+    {
+        return $aa;
+    }
 }
 
 
